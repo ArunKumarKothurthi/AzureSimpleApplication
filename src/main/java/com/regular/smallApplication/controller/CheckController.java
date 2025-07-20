0d832488c75429524a8046d0f293f79068bc3057
arunkumarkak3@gmail.com <you@example.com>
2025-07-20 22:42:15 +0530

    Update CheckController.java with new logic

--- src/main/java/com/regular/smallApplication/controller/CheckController.java
+++ src/main/java/com/regular/smallApplication/controller/CheckController.java
@@ -1,6 +1,7 @@
 package com.regular.smallApplication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
@@ -9,5 +10,11 @@ public class CheckController {
 	@GetMapping("basic")
 	public String basicStringMethod() {
 		return "Basic String Method";
+	}
+	
+	@GetMapping("hello/{name}")
+	public String namingMethod(@PathVariable("name")String name) {
+	
+		return "Hello "+name+" \n Babunava.....";
 	}
 }
